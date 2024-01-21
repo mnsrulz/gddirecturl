a2033906b24f335eb0a58453cc485254b450bedf
mnsrulz <manishrawat4u@gmail.com>
2024-01-21 23:14:49 +0000

    fix the package and using native fetch

--- index.js
+++ index.js
@@ -4,12 +4,6 @@
  * @public
  */
 
-const got = require('got');
-const cheerio = require('cheerio');
-const cookie = require('cookie');
-const setCookie = require('set-cookie-parser');
-const URL = require('url');
-
 exports.getMediaLink = getMediaLink;
 
 /**
@@ -19,54 +13,24 @@ exports.getMediaLink = getMediaLink;
  */
 async function getMediaLink(docId) {
     try {
-        const reqThumbnail = got.get('https://drive.google.com/thumbnail?sz=w320&id=' + docId, {
-            followRedirect: false
+        const reqThumbnail = fetch('https://drive.google.com/thumbnail?sz=w320&id=' + docId, {
+            redirect: 'manual'
         }).catch((x) => {
             return null;
         });
-        
-        const reqMedia = await got.get('https://drive.google.com/uc?id=' + docId + '&export=download', {
-            followRedirect: false
-        }).catch((x) => {
-            // if(x.statusCode && x.statusCode===404) return createFailedResponse(2, 'No resource found');
-            // return createFailedResponse(100, 'Unknown error');
-            return createFailedResponse(x.statusCode, x.message);
+        const directUrl = 'https://drive.usercontent.google.com/download?id=' + docId + '&export=download&confirm=1';
+        const reqMedia = await fetch(directUrl, {
+            method: 'head'
+        }).catch(x => {
+            return createFailedResponse(x.status, x.message);
         });
-        if (reqMedia.statusCode === 200) {
-            //we got something to work on.
+        if (reqMedia.ok) {
+            const thumbResponse = await reqThumbnail;
+            const thumbSource = (thumbResponse && thumbResponse.headers && thumbResponse.headers.get('location')) || '';
+            return createSuccessResponse(directUrl, thumbSource);
+        } else {
+            return createFailedResponse(reqMedia.status, reqMedia.statusText);
         }
-        else if (reqMedia.statusCode === 302) {
-            const u = URL.parse(reqMedia.headers.location);
-            if (u.hostname.endsWith('googleusercontent.com')) {
-                return createSuccessResponse(reqMedia.headers.location, null);
-            }
-            else {
-                //redirect occurs, possibly media not shared.
-                return createFailedResponse(401, 'Unauthorized');
-            }
-        }
-        else {
-            return reqMedia;
-        }
-        const responseCookies = setCookie.parse(reqMedia.headers["set-cookie"]);
-        const nextRequestCookies = responseCookies
-            .filter((x) => x.domain === '.drive.google.com')
-            .map((x) => {
-                return cookie.serialize(x.name, x.value);
-            }).join(';');
-
-        const $ = cheerio.load(reqMedia.body);
-        const nextPostLocation = $('form').attr('action');
-        const reqMediaConfirm = await got.post(nextPostLocation, {
-                headers: {
-                cookie: nextRequestCookies
-            }, followRedirect: false
-        });
-
-        const videoSource = reqMediaConfirm.headers.location;
-        const thumbResponse = await reqThumbnail;
-        const thumbSource = (thumbResponse && thumbResponse.headers && thumbResponse.headers.location) || '';
-        return createSuccessResponse(videoSource, thumbSource);
     } catch (error) {
         throw ('Error while fetching the media link.' + error);
     }
